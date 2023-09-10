479a9afd44128edf22501b7037291ed6a1b5ea22
Dan Nixon <dan@dan-nixon.com>
2023-09-10 18:47:51 +0100

    Tidy Nix config

--- flake.nix
+++ flake.nix
@@ -1,41 +1,32 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    allSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        devShell = pkgs.mkShell {
+          packages = (with pkgs; [
+            kapp
 
-    forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-      pkgs = import nixpkgs { inherit system; };
-    });
-  in
-  {
-    devShells = forAllSystems ({ pkgs }: {
-      default = pkgs.mkShell {
-        packages = (with pkgs; [
-          kapp
+            kubernetes-helm
 
-          kubernetes-helm
+            sops
+            vals
 
-          sops
-          vals
+            influxdb2-cli
+          ])
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
 
-          influxdb2-cli
-        ])
-        ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
-
-        shellHook = ''
-          export SOPS_PGP_FP="84E956241243C35EA286B410EA06B7ABA96D6BB8"
-          export KAPP_NAMESPACE="kapp"
-        '';
-      };
-    });
-  };
+          shellHook = ''
+            export SOPS_PGP_FP="84E956241243C35EA286B410EA06B7ABA96D6BB8"
+            export KAPP_NAMESPACE="kapp"
+          '';
+        };
+      }
+    );
 }
