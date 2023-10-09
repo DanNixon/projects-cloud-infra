08487c982601fc3c53686a05b516d408f64d583b
Dan Nixon <dan@dan-nixon.com>
2023-10-09 21:54:02 +0100

    Format Nix flake

--- flake.nix
+++ flake.nix
@@ -4,13 +4,17 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
       in rec {
         devShell = pkgs.mkShell {
-          packages = (with pkgs; [
+          packages = with pkgs; [
             kapp
 
             kubernetes-helm
@@ -19,8 +23,7 @@
             vals
 
             influxdb2-cli
-          ])
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
+          ];
 
           shellHook = ''
             export SOPS_PGP_FP="84E956241243C35EA286B410EA06B7ABA96D6BB8"
