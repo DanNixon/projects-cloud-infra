d60601f9aa6373844b88faaa8957f5d99606c204
Dan Nixon <dan@dan-nixon.com>
2024-04-08 14:52:49 +0100

    Update flake, add formatting tools

--- flake.nix
+++ flake.nix
@@ -1,35 +1,49 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in rec {
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [
-            kapp
+  }: let
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "aarch64-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Deployment tools
+          kapp
 
-            kubernetes-helm
+          # Manifest rendering tools
+          kubernetes-helm
 
-            sops
-            vals
+          # Secret management
+          sops
+          vals
 
-            influxdb2-cli
-          ];
+          # Code formatting tools
+          alejandra
+          treefmt
+          mdl
 
-          shellHook = ''
-            export SOPS_PGP_FP="84E956241243C35EA286B410EA06B7ABA96D6BB8"
-            export KAPP_NAMESPACE="kapp"
-          '';
-        };
-      }
-    );
+          # Application tools
+          influxdb2-cli
+        ];
+
+        shellHook = ''
+          export KAPP_NAMESPACE="kapp"
+          export SOPS_PGP_FP="84E956241243C35EA286B410EA06B7ABA96D6BB8"
+        '';
+      };
+    });
+  };
 }
